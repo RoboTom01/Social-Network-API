1b15537f17235f5af2196fc42e09d33fd28abd3f
Thomas <tmrtw8@gmail.com>
2023-05-23 22:53:10 -0600

    usercontrollerRough

---
+++ controllers/userController.js
@@ -0,0 +1,80 @@
+const { User, Thought } = require('../models');
+
+module.exports = {
+
+// getAllUsers,
+// getUser,
+// createUser,
+// updateUser,
+// deleteUser,
+// addFriend,
+// removeFriend
+
+  // Get all users
+  async getAllUsers(req, res) {
+    try {
+      const userData = await User.find();
+      res.json(userData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  // Get a user
+  async getUser(req, res) {
+    try {
+      const userData = await User.findOne({ _id: req.params.userId })
+        .select('-__v');
+
+      if (!userData) {
+        return res.status(404).json({ message: 'No user with that ID' });
+      }
+
+      res.json(userData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  // Create a user
+  async createUser(req, res) {
+    try {
+      const userData = await User.create(req.body);
+      res.json(userData);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+  // Delete a user
+  async deleteUser(req, res) {
+    try {
+      const userData = await User.findOneAndDelete({ _id: req.params.userId });
+
+      if (!userData) {
+        res.status(404).json({ message: 'No user with that ID' });
+      }
+
+      await Thought.deleteMany({ _id: { $in: userData.thoughts } });
+      res.json({ message: 'user and thoughts deleted!' });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  // Update a user
+  async updateUser(req, res) {
+    try {
+      const userData = await User.findOneAndUpdate(
+        { _id: req.params.userId },
+        { $set: req.body },
+        { runValidators: true, new: true }
+      );
+
+      if (!userData) {
+        res.status(404).json({ message: 'No user with this id!' });
+      }
+
+      res.json(userData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+};
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,20 +1,24 @@
 const router = require('express').Router();
 const {
-  getCourses,
-  getSingleCourse,
-  createCourse,
-  updateCourse,
-  deleteCourse,
-} = require('../../controllers/courseController.js');
+getAllUsers,
+// getUser,
+createUser,
+// updateUser,
+// deleteUser,
+// addFriend,
+// removeFriend
+} = require('../../controllers/userController.js');
 
-// /api/courses
-router.route('/').get(getCourses).post(createCourse);
+// /api/users
+router.route('/').get(getAllUsers).post(createUser);
 
-// /api/courses/:courseId
-router
-  .route('/:courseId')
-  .get(getSingleCourse)
-  .put(updateCourse)
-  .delete(deleteCourse);
+// /api/courses/:userId
+// router
+//   .route('/:userId')
+//   .get(getSingleCourse)
+//   .put(updateCourse)
+//   .delete(deleteCourse);
+
+// /api/courses/:userId/friends/:friendID
 
 module.exports = router;
